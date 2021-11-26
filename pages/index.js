8bd6f103c533e6785e529f61b5bbb9662d80a03e
Jeffrey-Biles-Envoy <jeffrey@envoy.com>
2021-11-26 01:12:04 -0600

    Click button to add component

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,63 @@
 import Head from 'next/head'
+import { useState } from 'react'
 
 export default function Home() {
+  const [exampleJSON, editExampleJSON] = useState([
+    {
+      type: 'button',
+      text: "It's my button"
+    },
+    {
+      type: 'text',
+      text: "I can make text"
+    },
+    {
+      type: 'header',
+      text: 'and headers'
+    }
+  ])
+
+  const renderComponent = function(c) {
+    if(c.type == 'button') {
+      return (
+        <button>{c.text}</button>
+      )
+    } else if(c.type == 'text') {
+      return (
+        <p>{c.text}</p>
+      )
+    } else if(c.type == 'header') {
+      return (
+        <h2>{c.text}</h2>
+      )
+    }
+  }
+
+  const addText = function(){
+    editExampleJSON([...exampleJSON, {type: 'text', text: "I added some text"}])
+  }
+  const addHeader = function(){
+    editExampleJSON([...exampleJSON, {type: 'header', text: 'I added a header'}])
+  }
+  const addButton = function(){
+    editExampleJSON([...exampleJSON, {type: 'button', text: 'I added a button'}])
+  }
+
   return (
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className="left-bar">
+            <button onClick={addText}>+ text</button>
+            <button onClick={addHeader}>+ header</button>
+            <button onClick={addButton}>+ button</button>
+          </div>
+          <div className="preview">
+            {exampleJSON.map(c => renderComponent(c))}
+          </div>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
@@ -74,25 +74,6 @@ export default function Home() {
           flex: 1;
           display: flex;
           flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
           justify-content: center;
           align-items: center;
         }
@@ -148,38 +129,16 @@ export default function Home() {
           margin-top: 3rem;
         }
 
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
+        .left-bar {
+          width: 120px;
         }
 
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
+        .left-bar button {
+          width: 90%;
         }
 
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
+        .preview {
+          flex-grow: 4;
         }
 
         @media (max-width: 600px) {
@@ -202,6 +161,12 @@ export default function Home() {
 
         * {
           box-sizing: border-box;
+        }
+
+        button {
+          cursor: pointer;
+          padding: 8px;
+          margin-top: 8px;
         }
       `}</style>
     </div>
